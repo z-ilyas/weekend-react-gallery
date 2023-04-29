a7f9f93ccee5541fc82af30c1ae92c9aebb27c97
Ilyas Mohamed <zswaleh@ymail.com>
2023-04-28 19:18:28 -0500

    click now flips the image and shows description

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -4,30 +4,39 @@ import axios from 'axios';
 function GalleryItem (props) {
 
 const [Description, setDescription] = useState(false);
+
 const flipImage = () => {
+    console.log('Clicked');
     if(Description){
         setDescription(false)
     }else{
         setDescription(true)
     }
+    console.log(Description);
 }
 
-const showStuff = (pics) => {
-    return (<img src = {pics}
-    />
-)
-    }
+const showStuff = (pics, descriptions) => {
+if(!Description){
+    return(
+        < div className='gallery'>
+        <img src = {pics} 
+            onClick={flipImage}
+        />
+        </div>
+    )
+}else{
+    return(
+        < div className='gallery'>
+        {descriptions}
+        </div>
+    )
+}
+}
 
-// }else{
-//     return (
-//     <>
-//         {descriptions}
-//     </>
-// )}
 
 return (
         <>
-    {showStuff(props.pic.path)}
+        {showStuff(props.pic.path, props.pic.description)}
         </>
     )
 }
